19d6ce43a2e028c989fb2411a9b9b75996bbc93b
mohamedElsayed1181 <mohamedelsayed908070@gmail.com>
2025-07-23 16:44:33 +0300

    add loading component to use in any project

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import './i18n';
+import { Provider } from 'react-redux';
+import { store } from './store/index.ts';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+  <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
---
+++ src/shared/Loading/Loading.tsx
@@ -0,0 +1,8 @@
+import "./Loading.css";
+export default function Loading() {
+  return (
+    <div className="mt-5 py-5">
+      <div className="loading-spinner"></div>
+    </div>
+  );
+}
